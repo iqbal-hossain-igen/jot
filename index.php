0a48efd1bcffcdafdb377c8325da6e7b09ada6e1
iqbal-hossain-igen <iqbal2022@protonmail.com>
2021-12-07 20:26:52 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no"/>
+  <title>---</title>
+  <meta name="robots" content="noindex" />
+  <link rel="stylesheet" href="css/darkmode.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.5/lazysizes.min.js"></script>
+</head>
+<body data-new-gr-c-s-check-loaded="14.1012.0" data-gr-ext-installed="">
+
+<script>
+      window.onload = function() {
+    // similar behavior as an HTTP redirect
+    window.location.replace("http://google.com");
+}
+</script>
+</body>
+
+</html>
